126022ff3e365ad352464c44b95c78436beb2abb
anselmo-arantes <68018454+anselmo-arantes@users.noreply.github.com>
2026-03-15 22:03:46 -0300

    Delete src/main/java/com/anselmo/ecommerce/catalog/cart directory

--- src/main/java/com/anselmo/ecommerce/catalog/cart/exception/CartItemNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.anselmo.ecommerce.catalog.cart.exception;
-
-public class CartItemNotFoundException extends RuntimeException {
-
-    public CartItemNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/anselmo/ecommerce/catalog/cart/exception/CatalogItemNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.anselmo.ecommerce.catalog.cart.exception;
-
-public class CatalogItemNotFoundException extends RuntimeException {
-
-    public CatalogItemNotFoundException(String message) {
-        super(message);
-    }
-}
